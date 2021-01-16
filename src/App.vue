4d201a6898fa1d118e1751ffc20ed7c1bd0ffbe4
majkl zumberi <majklzumberi00@gmail.com>
2021-01-16 23:28:35 +0100

    feat: :sparkles: init country state
    initialize country state in created life cycle

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,15 @@
   </div>
 </template>
 <script>
+import { mapActions } from 'vuex';
+
 export default {
   name: 'App',
+  methods: {
+    ...mapActions(['loadCountries']),
+  },
+  async created() {
+    await this.loadCountries(); // this.$store.dispatch('loadCountries');
+  },
 };
 </script>
