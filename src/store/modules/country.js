cd6df6226fd8df157156bb28e49849cbe2262d89
majkl zumberi <majklzumberi00@gmail.com>
2021-01-16 23:23:09 +0100

    feat(store): :sparkles: add country store
    added store (default state for loading, countries), added getters, mutations and actions
    
    initialize and store countries in vue state

---
+++ src/store/modules/country.js
@@ -0,0 +1,48 @@
+import axios from 'axios';
+import Vue from 'vue';
+
+const state = {
+  loading: false,
+  countries: [],
+};
+
+const getters = {
+  // eslint-disable-next-line no-shadow
+  isLoading(state) {
+    return state.loading;
+  },
+};
+
+const mutations = {
+  // eslint-disable-next-line no-shadow
+  storeCountries(state, countries) {
+    Vue.set(state, 'countries', countries);
+  },
+  // eslint-disable-next-line no-shadow
+  setLoading(state, status) {
+    state.loading = status;
+  },
+};
+
+const actions = {
+  async loadCountries({ commit }) {
+    try {
+      commit('setLoading', true);
+      const [{ data }] = await Promise.all([
+        axios.get('https://restcountries.eu/rest/v2/region/europe'),
+        new Promise((resolve) => setTimeout(resolve, 600)),
+      ]);
+      const formattedCountries = data.map(({ name, capital, flag }) => ({ name, capital, flag }));
+      commit('storeCountries', { ...formattedCountries });
+    } finally {
+      commit('setLoading', false);
+    }
+  },
+};
+
+export default {
+  state,
+  getters,
+  mutations,
+  actions,
+};
