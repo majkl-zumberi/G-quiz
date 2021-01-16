2f0b3d5d34be8434f1199ac414d599cbe6ffc54c
majkl zumberi <majklzumberi00@gmail.com>
2021-01-16 23:30:58 +0100

    chore: :heavy_plus_sign: add loading plugin
    added loading plugin for quasar conf

--- quasar.conf.js
+++ quasar.conf.js
@@ -102,7 +102,9 @@ module.exports = function (/* ctx */) {
       // directives: [],
 
       // Quasar plugins
-      plugins: [],
+      plugins: [
+        'Loading',
+      ],
     },
 
     // animations: 'all', // --- includes all animations
