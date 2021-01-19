f5ec5c60eba308ea4c402950015f60580cfd0633
majkl zumberi <majklzumberi00@gmail.com>
2021-01-19 22:21:52 +0100

    refactor(component): :construction: manage correct & wrong answer
    now the component handle button click, checks if the answer is either wrong or correct

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -8,20 +8,30 @@
     <template v-else>
       <CardQuestion
         :questionTitle=quizQuestionTitle
-        :questionOptions=quizQuestionOptions />
+        :questionOptions=quizQuestionOptions
+        :wrongAnsIdx=wrongIndex
+        :correctAnsidx=correctIndex />
     </template>
+    {{currentCorrectAnswer}}
   </q-page>
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
+import { mapGetters, mapActions } from 'vuex';
 import CardQuestion from 'components/CardQuestion';
+import eventBus from '../utils/eventBus';
 
 export default {
   name: 'PageIndex',
+  data() {
+    return {
+      correctIndex: -1,
+      wrongIndex: -1,
+    };
+  },
   components: { CardQuestion },
   computed: {
-    ...mapGetters(['isLoading', 'currentQuiz']),
+    ...mapGetters(['isLoading', 'currentQuiz', 'currentCorrectAnswer']),
     quizQuestionTitle() {
       const { name } = this.currentQuiz.quizQuestions || '';
       return `${name} is the capital of`;
@@ -29,6 +39,27 @@ export default {
     quizQuestionOptions() {
       return this.currentQuiz.quizOptions;
     },
+  },
+  methods: {
+    ...mapActions(['selectedOptionIndex', 'correctOptionIndex']),
+    async checkAnswer(selectedOption) {
+      // console.log('selezionato ', selectedOption);
+      // console.log(this.currentCorrectAnswer === selectedOption);
+      if (this.currentCorrectAnswer === selectedOption) { // correct answer
+        this.correctIndex = await this.selectedOptionIndex(selectedOption);
+      } else {
+        this.correctIndex = await this.correctOptionIndex();
+        this.wrongIndex = await this.selectedOptionIndex(selectedOption);
+      }
+      // console.log(await this.selectedOptionIndex(selectedOption));
+      // console.log('quello corretto', await this.correctOptionIndex());
+    },
+  },
+  created() {
+    eventBus.$on('optionSelected', this.checkAnswer);
+  },
+  beforeDestroy() {
+    eventBus.$off('optionSelected');
   },
 };
 </script>
