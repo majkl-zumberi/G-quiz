21828068ea9344ea7e732e060ab9945e87ed5644
majkl zumberi <majklzumberi00@gmail.com>
2021-01-19 23:48:18 +0100

    feat(component): :sparkles: add Result card

---
+++ src/components/Results.vue
@@ -0,0 +1,45 @@
+<template>
+  <q-card style="border-radius:25px">
+      <q-card-section class="q-pa-lg">
+        <img src="./../assets/undraw_winners_ao2o2.svg" alt="summary">
+      </q-card-section>
+      <q-card-section>
+        <div class="question text-h6 indigo-4 text-weight-bold text-center">
+          Results
+        </div>
+      </q-card-section>
+      <q-card-section>
+        <div class="question text-h6 indigo-4 text-weight-bold text-center">
+          you got {{guessed}} correct answers
+        </div>
+      </q-card-section>
+      <q-card-actions align="center">
+       <q-btn
+        outline
+        color="indigo-4"
+        label="Try Again"
+        class="q-px-md"
+        @click="resetQuiz"
+        />
+      </q-card-actions>
+    </q-card>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex';
+
+export default {
+  name: 'results',
+  computed: {
+    ...mapGetters(['guessed']),
+  },
+  methods: {
+    ...mapActions(['resetGame', 'loadCountries', 'shuffleQuesitons']),
+    async resetQuiz() {
+      this.resetGame();
+      await this.loadCountries(); // this.$store.dispatch('loadCountries');
+      await this.shuffleQuesitons();
+    },
+  },
+};
+</script>
