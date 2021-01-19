926c68a39f73f7f12f53cf60649144ae1b693f20
majkl zumberi <majklzumberi00@gmail.com>
2021-01-19 23:50:33 +0100

    feat(component): :sparkles: use Record component

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -5,20 +5,22 @@
         v-if="isLoading"
         size="3em"
         color="orange" />
-    <template v-else>
+    <template v-else-if="!isGameOver">
       <CardQuestion
         :questionTitle=quizQuestionTitle
         :questionOptions=quizQuestionOptions
         :wrongAnsIdx=wrongIndex
-        :correctAnsidx=correctIndex />
+        :correctAnsidx=correctIndex
+        @nextTurn="nextTurn" />
     </template>
-    {{currentCorrectAnswer}}
+    <Results v-else/>
   </q-page>
 </template>
 
 <script>
 import { mapGetters, mapActions } from 'vuex';
 import CardQuestion from 'components/CardQuestion';
+import Results from 'components/Results';
 import eventBus from '../utils/eventBus';
 
 export default {
@@ -29,9 +31,9 @@ export default {
       wrongIndex: -1,
     };
   },
-  components: { CardQuestion },
+  components: { CardQuestion, Results },
   computed: {
-    ...mapGetters(['isLoading', 'currentQuiz', 'currentCorrectAnswer']),
+    ...mapGetters(['isLoading', 'currentQuiz', 'currentCorrectAnswer', 'isGameOver']),
     quizQuestionTitle() {
       const { name } = this.currentQuiz.quizQuestions || '';
       return `${name} is the capital of`;
@@ -41,18 +43,24 @@ export default {
     },
   },
   methods: {
-    ...mapActions(['selectedOptionIndex', 'correctOptionIndex']),
+    ...mapActions(['selectedOptionIndex', 'correctOptionIndex', 'userGuessed', 'incrementAndCheckTurn']),
     async checkAnswer(selectedOption) {
       // console.log('selezionato ', selectedOption);
       // console.log(this.currentCorrectAnswer === selectedOption);
       if (this.currentCorrectAnswer === selectedOption) { // correct answer
         this.correctIndex = await this.selectedOptionIndex(selectedOption);
-      } else {
+        this.userGuessed();
+      } else { // wrong answer
         this.correctIndex = await this.correctOptionIndex();
         this.wrongIndex = await this.selectedOptionIndex(selectedOption);
       }
       // console.log(await this.selectedOptionIndex(selectedOption));
       // console.log('quello corretto', await this.correctOptionIndex());
+    },
+    nextTurn() {
+      this.correctIndex = -1;
+      this.wrongIndex = -1;
+      this.incrementAndCheckTurn();
     },
   },
   created() {
