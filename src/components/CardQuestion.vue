722be8022bb4c741142252de14d8a98fd2ae4c48
majkl zumberi <majklzumberi00@gmail.com>
2021-01-19 20:22:57 +0100

    feat(component): :sparkles: handle current question & options
    based on turn this component render the question and the related options

---
+++ src/components/CardQuestion.vue
@@ -0,0 +1,34 @@
+<template>
+  <q-card style="border-radius:25px">
+      <q-card-section>
+        <div class="question text-h6 indigo-4 text-weight-bold">
+          {{questionTitle}}
+        </div>
+      </q-card-section>
+      <q-card-section class="q-pa-lg">
+        <ButtonChoice
+          v-for="(choice,idx) in questionOptions"
+           :key="idx"
+           :capital="choice" />
+
+      </q-card-section>
+    </q-card>
+</template>
+
+<script>
+import ButtonChoice from 'components/ButtonChoice';
+
+export default {
+  name: 'card-question',
+  components: {
+    ButtonChoice,
+  },
+  props: ['questionTitle', 'questionOptions'],
+};
+</script>
+
+<style lang="scss" scoped>
+  .question{
+    color: $indigo-8;
+  }
+</style>
