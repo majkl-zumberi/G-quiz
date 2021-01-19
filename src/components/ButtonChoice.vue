47c2d6ae06e1bb5d2c0dfede6a5611303359438e
majkl zumberi <majklzumberi00@gmail.com>
2021-01-19 20:21:47 +0100

    feat(component): :sparkles: add button choice for question option

---
+++ src/components/ButtonChoice.vue
@@ -0,0 +1,20 @@
+<template>
+  <q-btn
+          outline
+          rounded
+          color="deep-purple-4"
+          size="lg"
+          :label="capital"
+          style="width:100%;margin-bottom:.5em"/>
+</template>
+<script>
+export default {
+  name: 'buttonChoice',
+  props: {
+    capital: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
