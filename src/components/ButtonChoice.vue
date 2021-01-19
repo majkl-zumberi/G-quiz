d9f7466e87d84ab0c5792c4bd59c7640745268a6
majkl zumberi <majklzumberi00@gmail.com>
2021-01-19 22:18:15 +0100

    perf(component): :sparkles: buttonChoice manage from props
    the button component is now fullt managed from the props; e handle click via event Emitter

--- src/components/ButtonChoice.vue
+++ src/components/ButtonChoice.vue
@@ -1,19 +1,35 @@
 <template>
   <q-btn
-          outline
+          :outline="status==='default'"
           rounded
-          color="deep-purple-4"
+          :color="status==='default'?'deep-purple-4':status==='correct'?'green-13':'red-13'"
           size="lg"
+          :disable="disabled"
           :label="capital"
+          @click="emitValue"
           style="width:100%;margin-bottom:.5em"/>
 </template>
 <script>
+import eventBus from '../utils/eventBus';
+
 export default {
   name: 'buttonChoice',
   props: {
+    /**
+     * default for default state of the button
+     * correct for correct answer
+     * wrong for wrong answer
+     */
+    status: String,
+    disabled: Boolean,
     capital: {
       type: String,
       required: true,
+    },
+  },
+  methods: {
+    emitValue() {
+      eventBus.$emit('optionSelected', this.capital);
     },
   },
 };
